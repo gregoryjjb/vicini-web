4805bbf142d0c3c9aba4acd64665d4bfb52335c7
Greg Brisebois <Gregory.Brisebois@analog.com>
2018-07-02 15:22:54 -0700

    Added some desired plugin sample features

--- src/plugins/LTC1234.js
+++ src/plugins/LTC1234.js
@@ -5,25 +5,49 @@ export default {
 	
 	fields: [{
 		name: 'volts',
-		label: 'Volts',
+		label: 'Voltage (mv)',
 		type: 'number',
+		units: 'mV',
+		unitScale: 0.001,
 		defaultValue: 420,
+		output: false,
 	}, {
 		name: 'text',
 		label: 'Volts but doubled',
 		type: 'text',
 		output: true,
+	}, {
+		name: 'linOutput',
+		label: 'Linduino Output',
+		output: true,
+	}, {
+		name: 'blabla',
+		type: 'button',
+		onClick: (values) => {
+			return sendLinduinoString(
+				`Please set the volts to ${values.volts}`,
+				linduinoValues => {
+					return {
+						linOutput: linduinoValues.volts,
+					}
+				}
+			);
+			
+			return {
+				volts: 420,
+			};
+		},
 	}],
+	
 	
 	reducer: function(oldValues) {
 		
-		console.log("REDUCER CALLED:", oldValues);
+		//console.log("REDUCER CALLED:", oldValues);
 		
 		let { volts } = oldValues;
 		
 		return {
-			...oldValues,
-			text: `${volts * 2}`,
+			text: volts * 2 + ' mV',
 		};
 	},
 }
