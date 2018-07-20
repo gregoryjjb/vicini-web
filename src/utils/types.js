85adbadd8e244975553e85142538295cf0acdc69
Greg Brisebois <gregoryjjb@gmail.com>
2018-07-20 16:11:38 -0700

    Clarified plugin type

--- src/utils/types.js
+++ src/utils/types.js
@@ -4,10 +4,19 @@ export const hardwareType = PropTypes.shape({
     name: PropTypes.string.isRequired,
 })
 
+const stringOrNumber = PropTypes.oneOfType([PropTypes.string, PropTypes.number]);
+
 export const fieldType = PropTypes.shape({
     name: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
     label: PropTypes.string,
+    units: PropTypes.string,
+    group: PropTypes.string,
+    defaultValue: stringOrNumber,
+    options: PropTypes.arrayOf(PropTypes.shape({
+        value: stringOrNumber.isRequired,
+        label: PropTypes.string.isRequired,
+    })),
     output: PropTypes.bool,
     onClick: PropTypes.func,
 })
