a1f5e487e74bc13bf3178e9d9d558b1e9a862551
Greg Brisebois <gregoryjjb@gmail.com>
2018-07-25 23:50:58 -0700

    Added theme to window scope

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,15 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
-const getTheme = mode => createMuiTheme({
-	palette: {
-		type: mode,
-	}
-})
+const getTheme = mode => {
+	let theme = createMuiTheme({
+		palette: {
+			type: mode,
+		}
+	})
+	
+	window.theme = theme;
+	
+	return theme;
+}
 
 export default getTheme;
