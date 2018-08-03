de868c37b82303278325ec4b2545ec2d279621b5
Greg Brisebois <gregoryjjb@gmail.com>
2018-08-03 12:31:12 -0700

    Theme change: grey and green

--- src/theme.js
+++ src/theme.js
@@ -4,6 +4,12 @@ const getTheme = mode => {
 	let theme = createMuiTheme({
 		palette: {
 			type: mode,
+			primary: {
+				main: '#455a64',
+			},
+			secondary: {
+				main: '#66bb6a',
+			},
 		},
 		mixins: {
 			verticalSlice: {
